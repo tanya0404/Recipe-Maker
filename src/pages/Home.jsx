d53c93c1aa0ed8ceed59d8c2f9698357f3d1c2ee
tanya0404 <149752404+tanya0404@users.noreply.github.com>
2025-06-03 21:51:12 +0530

    update3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
-
+import axios from "axios";
 const Home = () => {
+
+  const getproduct = async()=> {
+    try{
+      const {data} =await axios.get("https://fakestoreapi.com/products/1");
+      console.log(data);
+     }
+    catch(error){
+      console.log(error);
+    }
+  }
+
   return (
+    
     <div>
       <h2>home</h2>
+      <button>Get Product</button>
     </div>
   )
 }
 
-export default Home
+export default Home;
